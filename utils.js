6e64a43495a2cec6708d9c9316ea875f1f576bc0
hrafnhilduro <hoh44@hi.is>
2019-03-07 23:03:56 +0000

    test

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 /**
  * Higher-order fall sem umlykur async middleware með villumeðhöndlun.
  *
- * @param {function} fn Middleware sem grípa á villur fyrir
+ * @param {function} fn Middleware sem á að grípa villur fyrir
  * @returns {function} Middleware með villumeðhöndlun
  */
 function catchErrors(fn) {
